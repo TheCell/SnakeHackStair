3d9a1c4c3496e003e024f388273053f36ad9cd26
Stillhart Patrick iastillh <patrick.stillhart@stud.hslu.ch>
2017-11-11 10:35:20 +0100

    fancy message

--- index.js
+++ index.js
@@ -9,61 +9,69 @@ var logger = require('morgan')
 var app = express()
 var routes = require('./routes')
 
+
+console.log(`
+****************************************
+      NEXT ROUND
+****************************************
+
+  `)
+
 // For deployment to Heroku, the port needs to be set using ENV, so
 // we check for the port number in process.env
 app.set('port', (process.env.PORT || 9001))
 
 app.enable('verbose errors')
 
-app.use(logger('dev'))
+//app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(routes)
 
-app.use('*',function (req, res, next) {
-  if (req.url === '/favicon.ico') {
-    // Short-circuit favicon requests
-    res.set({'Content-Type': 'image/x-icon'})
-    res.status(200)
-    res.end()
-    next()
-  } else {
-    // Reroute all 404 routes to the 404 handler
-    var err = new Error()
-    err.status = 404
-    next(err)
-  }
+app.use('*', function(req, res, next) {
+	if (req.url === '/favicon.ico') {
+		// Short-circuit favicon requests
+		res.set({ 'Content-Type': 'image/x-icon' })
+		res.status(200)
+		res.end()
+		next()
+	} else {
+		// Reroute all 404 routes to the 404 handler
+		var err = new Error()
+		err.status = 404
+		next(err)
+	}
 
-  return
+	return
 })
 
 // 404 handler middleware, respond with JSON only
-app.use(function (err, req, res, next) {
-  if (err.status !== 404) {
-    return next(err)
-  }
+app.use(function(err, req, res, next) {
+	if (err.status !== 404) {
+		return next(err)
+	}
 
-  res.status(404)
-  res.send({
-    status: 404,
-    error: err.message || "These are not the snakes you're looking for"
-  })
+	res.status(404)
+	res.send({
+		status: 404,
+		error: err.message || "These are not the snakes you're looking for"
+	})
 
-  return
+	return
 })
 
 // 500 handler middleware, respond with JSON only
-app.use(function (err, req, res, next) {
-  var statusCode = err.status || 500
+app.use(function(err, req, res, next) {
+	var statusCode = err.status || 500
 
-  res.status(statusCode)
-  res.send({
-    status: statusCode,
-    error: err
-  })
+	res.status(statusCode)
+	res.send({
+		status: statusCode,
+		error: err
+	})
 
-  return
+	return
 })
 
-var server = app.listen(app.get('port'), function () {
-  console.log('Server listening on port %s', app.get('port'))
+var server = app.listen(app.get('port'), function() {
+	console.log('Server listening on port %s', app.get('port'))
 })
